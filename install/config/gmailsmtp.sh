9676cfe08814cded5ab40e6c5447b34aee90473f
Dashamir Hoxha <dashohoxha@gmail.com>
2014-01-25 13:20:53 +0000

    Improved email config script.

--- install/config/gmailsmtp.sh
+++ install/config/gmailsmtp.sh
@@ -29,15 +29,16 @@ sed -i /etc/ssmtp/revaliases \
     -e "/^root:/ c root:$GMAIL:smtp.gmail.com:587" \
     -e "/^admin:/ c admin:$GMAIL:smtp.gmail.com:587"
 
-sed -i /etc/apache2/sites-available/default \
-    -e "s/ServerAdmin .*\$/ServerAdmin $GMAIL/"
-sed -i /etc/apache2/sites-available/default-ssl \
-    -e "s/ServerAdmin .*\$/ServerAdmin $GMAIL/"
+for file in $(ls /etc/apache2/sites-available/)
+do
+    sed -i /etc/apache2/sites-available/$file -e "s/ServerAdmin .*\$/ServerAdmin $GMAIL/"
+    sed -i /etc/apache2/sites-available/$file -e "s/ServerAdmin .*\$/ServerAdmin $GMAIL/"
+done
 
 ### modify drupal variables that are used for sending email
 echo "Modifying drupal variables that are used for sending email..."
 $(dirname $0)/mysqld.sh start
-drush @lbd php-script $(dirname $0)/gmailsmtp.php "$GMAIL" "$PASSWD"
+drush @local php-script $(dirname $0)/gmailsmtp.php "$GMAIL" "$PASSWD"
 
 ### drush may create css/js files with wrong(root) permissions
 rm -rf /var/www/lbd/sites/default/files/css/
