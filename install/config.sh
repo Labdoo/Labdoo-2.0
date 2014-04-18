b90f152083f8d351988714a406df8fb5f88b86a6
jrosgiralt <jrosgiralt@gmail.com>
2014-04-18 12:19:36 +0000

    adjusted order of configuring Labdoo

--- install/config.sh
+++ install/config.sh
@@ -5,8 +5,8 @@ cwd=$(dirname $0)
 $cwd/config/domain.sh
 $cwd/config/mysql_passwords.sh
 $cwd/config/mysql_labdoo.sh
-$cwd/config/drupalpass.sh
 $cwd/config/labdoo.sh en
+$cwd/config/drupalpass.sh
 $cwd/config/gmailsmtp.sh
 
 $cwd/config/mysqld.sh stop
