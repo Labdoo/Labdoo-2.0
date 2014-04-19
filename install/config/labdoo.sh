a6fb6bad6e2a97b08c11fd5f69cabc87689b6e31
jrosgiralt <jrosgiralt@gmail.com>
2014-04-19 16:52:49 +0000

    ---migrated hook_install to hook_enable for visualize and communicate modules---fixed warning when creating sample objects

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -5,6 +5,5 @@
 enOrDis=$1
 
 $(dirname $0)/mysqld.sh start
-drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communicate \
-                       lbd_visualize 
-#                      lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hubs 
+drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communicate\
+                       lbd_visualize # lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hubs 
