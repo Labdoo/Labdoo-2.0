def35612ce1f62836258ce59ce7a0269a6949555
jrosgiralt <jrosgiralt@gmail.com>
2014-04-27 20:44:16 +0000

    Implemented geolocation maps of the various Labdoo objects | Implemented a feature for Labdoo blocks | enabled ip_geoloc

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -7,3 +7,7 @@ enOrDis=$1
 $(dirname $0)/mysqld.sh start
 drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communicate\
                        lbd_visualize # lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hubs 
+
+# FIXME: somehow this lib needs a reinstall (so that the nodes created by .install are correct) 
+drush @lbd dis labdoo_lib
+drush @lbd en labdoo_lib
