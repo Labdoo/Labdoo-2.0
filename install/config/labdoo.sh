a2831079983609269475a156794780fd73451d50
jrosgiralt <jrosgiralt@gmail.com>
2014-04-28 08:19:58 +0200

    disabled default installation of sample nodes

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -9,7 +9,7 @@ drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communica
                        lbd_visualize lbd_blocks 
 
 # Some sample nodes for development
-drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hub 
+#drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hub 
 
 # FIXME: somehow this lib needs a reinstall (so that the nodes created by labdoo_lib.install are correct) 
 drush @lbd dis -y labdoo_lib
