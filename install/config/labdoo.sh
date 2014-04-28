a24a1b7107f70cd8bb6e8dd67b6b6536fb5159fe
jrosgiralt <jrosgiralt@gmail.com>
2014-04-28 11:11:04 +0200

    Fixed lbd_communicate dependency

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -8,9 +8,11 @@ $(dirname $0)/mysqld.sh start
 drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communicate\
                        lbd_visualize lbd_blocks 
 
-# Some sample nodes for development
-#drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hub 
-
-# FIXME: somehow this lib needs a reinstall (so that the nodes created by labdoo_lib.install are correct) 
+# FIXME: Somehow labdoo_lib needs a reinstall (so that the nodes created by labdoo_lib.install are correct) 
+# Remember to enable back labdoo_lib and all the libraries that depend on it
 drush @lbd dis -y labdoo_lib
-drush @lbd en -y labdoo_lib
+drush @lbd en -y labdoo_lib lbd_communicate
+
+# Some sample nodes for development
+drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hub 
+
