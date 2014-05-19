27d05caeb39bc453bf7241d4b28a622cb9b812c9
jrosgiralt <jrosgiralt@gmail.com>
2014-05-19 07:09:27 +0000

    Adding the option to configure the email address of user=1 'admin'

--- install/config/drupalpass.sh
+++ install/config/drupalpass.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-### Set the admin password of Drupal.
+### Set the admin password and email address of Drupal.
 
 echo "
-===> Please enter new password for the Drupal 'admin' account.
+===> Please enter the email address of the 'admin' user account in Drupal"
+
+MAIL='contact@labdoo.org'
+read -p "Enter the email address [$MAIL]: " input
+MAIL=${input:-$MAIL}
+
+drush sqlq "update users set mail='$MAIL' where uid=1"
+
+echo "
+===> Please enter new password for the 'admin' user account in Drupal.
 "
 stty -echo
 read -p "Enter the password: " passwd
