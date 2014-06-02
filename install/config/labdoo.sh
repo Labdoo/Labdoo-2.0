12ef39f4da0f0f084a0c58b28059d56e74999dbd
jrosgiralt <jrosgiralt@gmail.com>
2014-06-02 09:19:39 +0000

    ---labdoo action feature fixes

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -9,7 +9,7 @@ enOrDis=$1
 
 $(dirname $0)/mysqld.sh start
 drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communicate\
-                       lbd_visualize lbd_blocks lbd_dootrip 
+                       lbd_visualize lbd_dootrip lbd_actions lbd_blocks 
 
 # FIXME: Somehow labdoo_lib needs a reinstall (so that the nodes created by labdoo_lib.install are correct) 
 # Remember to enable back labdoo_lib and all the libraries that depend on it
