f8e854f718ab596bffd5b68d0d60de012609b830
jrosgiralt <jrosgiralt@gmail.com>
2014-06-07 11:28:31 +0000

    ---Additional explanations to various README.org files---Fixed division by zero---Enabled module lbd_dootrip---Enabled module views_slideshow_cycle---New block actions feed---New menu entry actions feed---Fixed permission access to "follow dooject" pop up

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -14,7 +14,7 @@ drush @lbd $enOrDis -y labdoo_lib lbd_content_types labdoo_objects lbd_communica
 # FIXME: Somehow labdoo_lib needs a reinstall (so that the nodes created by labdoo_lib.install are correct) 
 # Remember to enable back labdoo_lib and all the libraries that depend on it
 drush @lbd dis -y labdoo_lib
-drush @lbd en -y labdoo_lib lbd_communicate lbd_actions lbd_blocks
+drush @lbd en -y labdoo_lib lbd_communicate lbd_actions lbd_blocks lbd_dootrip
 
 # install smtp
 $cwd/gmailsmtp.sh
@@ -23,4 +23,8 @@ $cwd/gmailsmtp.sh
 # Note: these modules need to be installed after smtp is installed
 #       since they need to send out notification emails
 drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hub 
+
+# FIXME: for some reason module labdoo actions needs to be reinstalled to be properly activated
+drush @lbd dis -y lbd_actions
+drush @lbd en -y lbd_actions
 
