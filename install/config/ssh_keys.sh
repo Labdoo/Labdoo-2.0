f61f5aeca41b7918fee7b31a3bed8d7d76e432ee
Dashamir Hoxha <dashohoxha@gmail.com>
2014-10-22 23:12:26 +0200

    Small fix.

--- install/config/ssh_keys.sh
+++ install/config/ssh_keys.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ### generate ssh public/private keys
-mkdir ~/.ssh
+mkdir -p ~/.ssh
 chmod 700 ~/.ssh
+rm -f ~/.ssh/id_rsa
 ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ''
 mv ~/.ssh/{id_rsa.pub,authorized_keys}
