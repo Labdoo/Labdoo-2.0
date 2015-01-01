cbb7254e3aa064c13cadaafc3457b7fd7eca4213
jrosgiralt <jrosgiralt@gmail.com>
2015-01-01 18:38:43 +0000

    --- Disabled sample objects

--- install/config/labdoo.sh
+++ install/config/labdoo.sh
@@ -25,7 +25,8 @@ drush @lbd en -y labdoo_lib lbd_communicate lbd_actions lbd_dootrip
 # Some sample nodes for development (disable this for production)
 # Note: these modules need to be installed after smtp is installed
 #       since they need to send out notification emails
-drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hubs lbd_sample_dootrips 
+# [Disabled]
+#drush @lbd $enOrDis -y lbd_sample_doojects lbd_sample_edoovillages lbd_sample_hubs lbd_sample_dootrips 
 
 drush @lbd $enOrDis -y lbd_gmap
 echo "
