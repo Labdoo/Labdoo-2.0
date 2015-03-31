2c693cd54d2c2bdb4068342ca879425510c3af0c
jrosgiralt <jrosgiralt@gmail.com>
2015-03-31 04:38:26 +0000

    ---Fixed language switch for front page---Fixed nowrap feeds issue---Do not display TOC for single page wiki books---Added macro stats in user account pages---Fixed page title issue for 'user/login' path---Fixed scrollbar for 'follow dooject' button---Made 'Print tags for this dooject' translatable

--- themes/bootstrap1/template.php
+++ themes/bootstrap1/template.php
@@ -9,6 +9,8 @@
  *
  */
 function bootstrap1_preprocess_page(&$vars) {
+  global $user;
+
   // For superhub pages, we want to enable the title in the edit form so
   // that it can show up in the entity field of the block but we want to
   // hide the title when displaying the page.
@@ -18,4 +20,15 @@ function bootstrap1_preprocess_page(&$vars) {
   // Hide front page title
   if(!empty($vars['node']) && $vars['node']->title == 'Front Page')
     $vars['title'] = '';
+
+  if ($user->uid) {
+    // Logged in user
+  } else {
+    // Not logged in
+    // Fix the registration title done via the path /user/login 
+    // (which otherwise by default is "Home")
+    if (arg(0) == 'user' && arg(1) == 'login') {
+      drupal_set_title(t('User account'));
+    }
+  }
 }
