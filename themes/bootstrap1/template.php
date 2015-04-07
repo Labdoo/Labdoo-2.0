a811c2fabdc0d0b080cd743b83fc709bc1b67169
jrosgiralt <jrosgiralt@gmail.com>
2015-04-07 05:26:54 +0000

    Created menu items for share wall and various improvements/tunings

--- themes/bootstrap1/template.php
+++ themes/bootstrap1/template.php
@@ -17,8 +17,13 @@ function bootstrap1_preprocess_page(&$vars) {
   if(!empty($vars['node']) && in_array($vars['node']->type, array('superhub_page'))) {
     $vars['title'] = '';
   }
+
   // Hide front page title
   if(!empty($vars['node']) && $vars['node']->title == 'Front Page')
+    $vars['title'] = '';
+
+  // Hide share wall title
+  if(!empty($vars['node']) && $vars['node']->title == 'Share Wall')
     $vars['title'] = '';
 
   if ($user->uid) {
