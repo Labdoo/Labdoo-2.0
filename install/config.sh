B-Translator/old_btr_server

Config ssh keys.

install/config.sh
@@ -31,6 +31,7 @@ $btr/config/drupalpass.sh
 
 ### configurations for oauth2 login
 $btr/config/oauth2_login.sh @bcl @btr
+$bcl/config/ssh_keys.sh
 
 ### make clones btr_dev and bcl_dev
 if [ "$development" = 'true' ]

Labdoo/Labdoo-2.0

install/config.sh
@@ -17,6 +17,7 @@ $lbd/config/mysql_labdoo.sh
 $lbd/config/gmailsmtp.sh
 $lbd/config/labdoo.sh en
 $lbd/config/drupalpass.sh
+$lbd/config/ssh_keys.sh
 
 if [ "$development" = 'true' ]
 then