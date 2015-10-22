81a7e00a1f28aa9531f123588dfe91214f8406f4
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-22 11:54:24 +0200

    Make login expiration time of phpmyadmin longer

--- install/scripts/system-config.sh
+++ install/scripts/system-config.sh
@@ -111,3 +111,17 @@ Port $sshd_port
 PasswordAuthentication no
 X11Forwarding no
 EOF
+
+### config phpmyadmin
+if [ "$development" = 'true' ]
+then
+    ### make login expiration time longer
+    sed -i /etc/phpmyadmin/config.inc.php \
+        -e "/Don't expire login quickly/,$ d"
+    cat <<EOF >> /etc/phpmyadmin/config.inc.php
+// Don't expire login quickly
+\$sessionDuration = 60*60*24*7; // 60*60*24*7 = one week
+ini_set('session.gc_maxlifetime', \$sessionDuration);
+$cfg['LoginCookieValidity'] = \$sessionDuration;
+EOF
+fi
