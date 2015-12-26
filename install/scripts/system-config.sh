1c3b2f5a321b5cba9791b03ad6e2cfcd08f98f4f
jrosgiralt <jrosgiralt@gmail.com>
2015-12-26 10:41:28 +0000

    - Implements various improvements to the 'Dootronics' and 'Dootrips' dashboards based on https://www.labdoo.org/content/suggestions-improve-dootrips-dootrip-dashboard-and-dootronics-table-edoovillage.
    - Fixes 'printout of pages, containing the social network icons': https://www.labdoo.org/content/printout-pages-containing-social-network-icons
    
    - Improves boost cache size for better performance.

--- install/scripts/system-config.sh
+++ install/scripts/system-config.sh
@@ -12,7 +12,7 @@ sed -i /etc/fstab \
 cat <<EOF >> /etc/fstab
 ##### appended by installation scripts
 tmpfs		/dev/shm	tmpfs	defaults,noexec,nosuid	0	0
-tmpfs		/var/www/lbd/cache	tmpfs	defaults,size=5M,mode=0777,noexec,nosuid	0	0
+tmpfs		/var/www/lbd/cache	tmpfs	defaults,size=20M,mode=0777,noexec,nosuid	0	0
 devpts		/dev/pts	devpts	rw,noexec,nosuid,gid=5,mode=620		0	0
 # mount /tmp on RAM for better performance
 tmpfs /tmp tmpfs defaults,noatime,mode=1777,nosuid 0 0
