ed57330e5c2c592cd53574036a12fa17c552902b
jrosgiralt <jrosgiralt@gmail.com>
2016-01-24 05:11:20 +0000

    Fixed racing condition in Labdoo watchdog

--- utils/watchdog/labdoo-watchdog.sh
+++ utils/watchdog/labdoo-watchdog.sh
@@ -11,16 +11,21 @@ PATH=/bin:/usr/bin:/usr/sbin
 TEMPDIR=/tmp/labdoo-watchdog
 EMAIL=contact@labdoo.org
 
-if ( ! wget --no-check-certificate --timeout=15 -q -P $TEMPDIR http://localhost/robots.txt )
-then
-    # Server is down.
+#
+# This function restarts the Labdoo server checking that there is
+# no nother thread attempting to do the same,.
+#
+# $1 : A string containing the initial part of the email subject header
+#
+do_restart ()
+{
     # If already in the process of being recovered, don't proceed.
-    if [ ! -f ~/.lbd-being-recovered ]
+    if [ ! -f ~/.lbd-being-restarted ]
     then
         mkdir -p $TEMPDIR
-        touch ~/.lbd-being-recovered
+        touch ~/.lbd-being-restarted
         # Write an informative e-mail
-        echo -n "Subject: Labdoo server down | " > $TEMPDIR/mail
+        echo -n $1" " > $TEMPDIR/mail
         date >> $TEMPDIR/mail
         echo >> $TEMPDIR/mail
         echo >> $TEMPDIR/mail
@@ -32,15 +37,35 @@ then
         echo >> $TEMPDIR/mail
         # Restart labdoo
         echo "Now restarting Labdoo..." >> $TEMPDIR/mail
-        /etc/init.d/supervisor stop >> $TEMPDIR/mail 2>&1
-        /etc/init.d/supervisor start >> $TEMPDIR/mail 2>&1
+        # Do not run the supervisor with two separate commands
+        # 'stop' and 'start' because the stop is non-blocking
+        # and can collide with start. Instead, run it as 'restart'.
+        /etc/init.d/supervisor restart >> $TEMPDIR/mail 2>&1
         echo "Done." >> $TEMPDIR/mail
         # Send the mail
         echo >> $TEMPDIR/mail
         cat $TEMPDIR/mail
         cat $TEMPDIR/mail | sendmail $EMAIL
-        rm ~/.lbd-being-recovered
+        rm ~/.lbd-being-restarted
         rm -rf $TEMPDIR
     fi
+}
+
+
+if [ $# -eq 1 ] && [ $1 = "--restart-now" ]
+then
+    logger -s "Restarting the Labdoo engine now..."
+    do_restart "Subject: Labdoo is being restarted | "
+    logger -s "Done."
+else
+    # Check if server is alive
+    if ( ! wget --no-check-certificate --timeout=15 -q -P $TEMPDIR http://localhost/robots.txt )
+    then
+        logger -s "Labdoo engine is down. Restarting the engine now..."
+        do_restart "Subject: Labdoo server down | "
+        logger -s "Done."
+    fi
 fi
+
+exit
 
